6cba9982b83f55061af8cf0995cbdc0dd9567eb3
Ismail Prada <ismail.prada@gmail.com>
2024-04-04 15:01:15 +0200

    Fixing whitespace issues by using flair tokens.

--- ariadne/contrib/flair_recursive.py
+++ ariadne/contrib/flair_recursive.py
@@ -10,50 +10,73 @@ from pathlib import Path
 from cassis import Cas
 
 from flair.nn import Classifier as Tagger
-from flair.data import Sentence
+from flair.data import Sentence, Token
 
 from ariadne.classifier import Classifier
 from ariadne.contrib.inception_util import create_prediction, SENTENCE_TYPE, TOKEN_TYPE
 
 
 def fix_whitespaces(cas_tokens):
-    # build sentence with correct whitespaces
-    # (when using sentences, this should not be a problem afaik)
-    text = ""
-    last_end = 0
-    for cas_token in cas_tokens:
-        if cas_token.begin == last_end:
-            text += cas_token.get_covered_text()
+    """
+    Convert the CAS tokens to flair tokens with correct whitespaces.
+    """
+    text = []
+    for cas_token, following_cas_token in zip(cas_tokens, cas_tokens[1:] + [None]):
+        if following_cas_token is not None:
+            dist = following_cas_token.begin - cas_token.end
         else:
-            text += " " + cas_token.get_covered_text()
-        last_end = cas_token.end
+            dist = 1
+        token = Token(
+            cas_token.get_covered_text(),
+            whitespace_after=dist,
+            start_position=cas_token.begin
+        )
+        text.append(token)
     return text
 
 
-def predict_recursive(sent, tagger, cas, layer, feature, prev_sent="", offset=0):
-    if prev_sent == sent:
+def predict_recursive(sent, tagger, cas, layer, feature, prev_sent_len=None, offset=0):
+    if prev_sent_len == len(sent):
         return []
 
-    sent = Sentence(sent, use_tokenizer=False)
+    sent = Sentence(sent)
     
     tagger.predict(sent)
 
-    ents = sent.to_dict()["entities"]
+    #ents = sent.to_dict()["entities"]
+    ents = sent.get_spans()
 
     for entity in ents:
+        """
         entity["fixed_start_pos"] = entity["start_pos"] + offset
         entity["fixed_end_pos"] = entity["end_pos"] + offset
 
         begin = entity["fixed_start_pos"]
         end = entity["fixed_end_pos"]
-        label = entity["labels"][0]["value"]
+        """
+        begin = entity.start_position
+        end = entity.end_position
+        label = entity.tag
+        #print(label, entity.text, begin, end)  # RESTORE THIS FOR SERVER
         prediction = create_prediction(cas, layer, feature, begin, end, label)
         cas.add(prediction)
 
     for entity in ents:
-        if sorted(entity["labels"], key=lambda x: x["confidence"], reverse=True)[0]["value"] in ["head", "money", "date", "time.rec"]:  # NOTE IMPORTANT TO FIT THIS
+        if entity.tag in ["head", "money", "date", "time.rec"]:  # NOTE IMPORTANT TO FIT THIS
             continue
-        predict_recursive(entity["text"], tagger, cas, layer, feature, prev_sent=sent.to_dict()["text"], offset=offset + entity["start_pos"])
+        #if sorted(entity["labels"], key=lambda x: x["confidence"], reverse=True)[0]["value"] in ["head", "money", "date", "time.rec"]:  # NOTE IMPORTANT TO FIT THIS
+        #    continue
+        # copy the tokens so they can be used for the next recursion
+        tokens = []
+        for token in entity.tokens:
+            tokens.append(
+                Token(
+                    token.text,
+                    whitespace_after=token.whitespace_after,
+                    start_position=token.start_position
+                )
+            )
+        predict_recursive(tokens, tagger, cas, layer, feature, prev_sent_len=len(sent.tokens), offset=offset)
 
 
 class FlairRecursiveTagger(Classifier):
